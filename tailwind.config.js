a49e1d9f0bc61aec20476bb06800e301db8e1682
Jobsidney <jobsidney67@gmail.com>
2023-08-19 11:54:45 +0300

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/Pages/**/*.{html,js,tsx,jsx}","./src/components/**/*.{html,js,tsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
