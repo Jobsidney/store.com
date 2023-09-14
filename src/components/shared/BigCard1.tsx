2ceff55e588b83619bc17e38a9c046a15147c8a0
Jobsidney <jobsidney67@gmail.com>
2023-09-14 12:23:40 +0300

    big card 1 component

---
+++ src/components/shared/BigCard1.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+function BigCard1({}: Props) {
+  return (
+    <div className="min-w-[25%]">
+        <a href="../shop/shop.html" className=" ">
+            <img className=" h-[700px] border hover:border  hover:shadow-lg" src="https://transvelo.github.io/electro-html/2.0/assets/img/360X616/img1.jpg" alt="Image Description"/>
+        </a>
+    </div>
+  )
+}
+
+export default BigCard1
