d73a8e6d4cc7f0394c0b80c0d1c4089eebfa9f07
Jobsidney <jobsidney67@gmail.com>
2023-09-17 12:40:13 +0300

    gird to items and gap

--- src/components/shared/TopCategoryWeek.tsx
+++ src/components/shared/TopCategoryWeek.tsx
@@ -1,85 +1,127 @@
 import React from 'react'
+import TitleHalfBorder from './TitleHalfBorder'
 
 type Props = {}
 
 function TopCategoryWeek({}: Props) {
   return (
-    <div className=" bg-gray-50 px-[12%] py-24">
+    <div className=" bg-gray-50 px-[12%] py-16">
                 <div className="container">
-                    <div className=" d-flex justify-content-between border-bottom border-color-1 flex-lg-nowrap flex-wrap border-md-down-top-0 border-md-down-bottom-0 mb-5">
-                        <h3 className="section-title mb-0 pb-2 font-size-22">Top Categories this Week</h3>
+                <div className="flex-row flex justify-between border-b-2  border-[#dedfe0] ">
+                       <TitleHalfBorder title={""}/>
                     </div>
-                    <div className="flex-row flex flex-md-wrap overflow-auto overflow-md-visble">
-                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
-                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
-                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
-                                    <div className="media align-items-center">
-                                        <div className="pt-2">
-                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
-                                        </div>
-                                        <div className="ml-3 media-body">
-                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
-                                        </div>
-                                    </div>
-                                </a>
+                    <div className="grid grid-cols-5  justify-items-between gap-y-6  py-8">
+                    <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
-                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
-                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
-                                    <div className="media align-items-center">
-                                        <div className="pt-2">
-                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
-                                        </div>
-                                        <div className="ml-3 media-body">
-                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
-                                        </div>
-                                    </div>
-                                </a>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img10.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
-                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
-                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
-                                    <div className="media align-items-center">
-                                        <div className="pt-2">
-                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
-                                        </div>
-                                        <div className="ml-3 media-body">
-                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
-                                        </div>
-                                    </div>
-                                </a>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
-                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
-                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
-                                    <div className="media align-items-center">
-                                        <div className="pt-2">
-                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
-                                        </div>
-                                        <div className="ml-3 media-body">
-                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
-                                        </div>
-                                    </div>
-                                </a>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
-                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
-                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
-                                    <div className="media align-items-center">
-                                        <div className="pt-2">
-                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
-                                        </div>
-                                        <div className="ml-3 media-body">
-                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
-                                        </div>
-                                    </div>
-                                </a>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
                             </div>
                         </div>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="  bg-white hover:border-transparent  w-[270px] h-[120px] min-w-[270px]  hover:shadow-2xl lg:px-4 p-3">
+                            <div className="bg-white  flex flex-row items-center">
+                              
+                                <div className="">
+                                    <img className="w-full transform-rotate-15 w-[200px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/100X100/img4.png" alt="Image Description"/>
+                                </div>
+                                <div className="ml-3 media-body">
+                                    <h6 className="mb-0 text-gray-90 text-lg">Home &amp; Audio Enternteinment</h6>
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
