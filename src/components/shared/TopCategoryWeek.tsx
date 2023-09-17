82e193e2096e44bf580aade10c1250377bff23e5
Jobsidney <jobsidney67@gmail.com>
2023-09-17 11:54:12 +0300

    new category component added

---
+++ src/components/shared/TopCategoryWeek.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+type Props = {}
+
+function TopCategoryWeek({}: Props) {
+  return (
+    <div className=" bg-gray-50 px-[12%] py-24">
+                <div className="container">
+                    <div className=" d-flex justify-content-between border-bottom border-color-1 flex-lg-nowrap flex-wrap border-md-down-top-0 border-md-down-bottom-0 mb-5">
+                        <h3 className="section-title mb-0 pb-2 font-size-22">Top Categories this Week</h3>
+                    </div>
+                    <div className="flex-row flex flex-md-wrap overflow-auto overflow-md-visble">
+                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
+                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
+                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
+                                    <div className="media align-items-center">
+                                        <div className="pt-2">
+                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
+                                        </div>
+                                        <div className="ml-3 media-body">
+                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
+                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
+                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
+                                    <div className="media align-items-center">
+                                        <div className="pt-2">
+                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
+                                        </div>
+                                        <div className="ml-3 media-body">
+                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
+                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
+                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
+                                    <div className="media align-items-center">
+                                        <div className="pt-2">
+                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
+                                        </div>
+                                        <div className="ml-3 media-body">
+                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
+                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
+                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
+                                    <div className="media align-items-center">
+                                        <div className="pt-2">
+                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
+                                        </div>
+                                        <div className="ml-3 media-body">
+                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-3 col-xl-4 col-xl-2gdot4 mb-3 flex-shrink-0 flex-md-shrink-1">
+                            <div className="bg-white overflow-hidden shadow-on-hover h-100 d-flex align-items-center">
+                                <a href="../shop/product-categories-7-column-full-width.html" className="d-block pr-2 pr-wd-6">
+                                    <div className="media align-items-center">
+                                        <div className="pt-2">
+                                            <img className="img-fluid transform-rotate-15" src="https://transvelo.github.io/electro-html/2.0/assets/img/212X200/img1.jpg" alt="Image Description"/>
+                                        </div>
+                                        <div className="ml-3 media-body">
+                                            <h6 className="mb-0 text-gray-90">Home &amp; Audio Enternteinment</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+  )
+}
+
+export default TopCategoryWeek
