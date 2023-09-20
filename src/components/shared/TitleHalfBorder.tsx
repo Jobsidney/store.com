063123462c35036ae6378a5fd5d42b5ec22b01ca
Jobsidney <jobsidney67@gmail.com>
2023-09-20 09:29:59 +0300

    props passing

--- src/components/shared/TitleHalfBorder.tsx
+++ src/components/shared/TitleHalfBorder.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { types } from 'util'
 
-type Props = {
+type Props ={
   title:string
 }
 
-function TitleHalfBorder({title:string}: Props) {
+function TitleHalfBorder({title}: Props) {
   return (
     <div className=''>
-    <h3 className=" mb-0 pb-2 text-2xl">Smartphones &amp; Tablets</h3>
+    <h3 className=" mb-0 pb-2 text-2xl">{title}</h3>
     <div className='border-b-[3px] border-[#fed700] w-[114px]'></div>
 </div>
   )
