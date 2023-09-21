a3b9c8723c63e5a2f919eef41188e24b95812620
Jobsidney <jobsidney67@gmail.com>
2023-09-21 10:38:52 +0300

    price and some content

--- src/Pages/Products/ProductDetails.tsx
+++ src/Pages/Products/ProductDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as Bs from 'react-icons/bs'
 
 type Props = {}
 
@@ -6,7 +7,7 @@ function ProductDetails({}: Props) {
   return (
     <div className="px-[12%] py-16">
                     <div className="grid grid-cols-[40%,60%] gap-12 ">
-                        <div className="flex flex-col items-center">
+                        <div className="flex flex-col ">
                             <div className="">
                                 <div className="" >
                                     <img className="img-fluid" src="https://transvelo.github.io/electro-html/2.0/assets/img/720X660/img1.jpg" alt="Image Description"/>
@@ -65,34 +66,36 @@ function ProductDetails({}: Props) {
                             
                         </div>
                         <div className="col-md-7 mb-md-6 mb-lg-0">
-                            <div className="mb-2">
-                                <div className="border-bottom mb-3 pb-md-1 pb-3">
-                                    <a href="#" className="font-size-12 text-gray-5 mb-2 d-inline-block">Headphones</a>
-                                    <h2 className="font-size-25 text-lh-1dot2">Ultra Wireless S50 Headphones S50 with Bluetooth</h2>
+                            <div className="mb-2 text-gray-500">
+                                <div className="border-b mb-3  pb-3 space-y-5">
+                                    <a href="#" className="text-[15px] text-gray-500 font-thin">Headphones</a>
+                                    <h2 className="mb-0 pb-2 text-3xl text-black/70 font-light">Ultra Wireless S50 Headphones S50 with Bluetooth</h2>
                                     <div className="mb-2">
-                                        <a className="d-inline-flex align-items-center small font-size-15 text-lh-1" href="#">
-                                            <div className="text-warning mr-2">
-                                                <small className="fas fa-star"></small>
-                                                <small className="fas fa-star"></small>
-                                                <small className="fas fa-star"></small>
-                                                <small className="fas fa-star"></small>
-                                                <small className="far fa-star text-muted"></small>
+                                        <div  className="flex flex-row items-center space-x-2">
+                                            <div className="flex flex-row items-center text-[18px] space-x-1">
+                                                <Bs.BsStarFill color='#fed700'/>
+                                                <Bs.BsStarFill color='#fed700'/>
+                                                <Bs.BsStarFill color='#fed700'/>
+                                                <Bs.BsStarFill color='#fed700'/>
+                                                <Bs.BsStar className='text-gray-300'/>
                                             </div>
-                                            <span className="text-secondary font-size-13">(3 customer reviews)</span>
-                                        </a>
+                                            <span className="text-[15px] text-gray-500 ">(3 customer reviews)</span>
+                                        </div>
                                     </div>
-                                    <div className="d-md-flex align-items-center">
+                                    <div className="flex flex-row  items-center">
                                         <a href="#" className="max-width-150 ml-n2 mb-2 mb-md-0 d-block">
                                             <img className="w-[90px]" src="https://transvelo.github.io/electro-html/2.0/assets/img/720X660/img1.jpg" alt="Image Description"/></a>
-                                        <div className="ml-md-3 text-gray-9 font-size-14">Availability: <span className="text-green font-weight-bold">26 in stock</span></div>
+                                        <div className="ml-3 text-[16px] text-gray-500 ">Availability: <span className="text-[#4daa4d] font-bold">26 in stock</span>
+                                        </div>
                                     </div>
                                 </div>
+                                {/* whish list */}
                                 <div className="flex-horizontal-center flex-wrap mb-4">
-                                    <a href="#" className="text-gray-6 font-size-13 mr-2"><i className="ec ec-favorites mr-1 font-size-15"></i> Wishlist</a>
-                                    <a href="#" className="text-gray-6 font-size-13 ml-2"><i className="ec ec-compare mr-1 font-size-15"></i> Compare</a>
+                                    <a href="" className="text-gray-400 font-size-13 flex fex-row space-x-2 items-center hover:text-black "><Bs.BsHeart size={20} /> <span>Wishlist</span> </a>
+                                   
                                 </div>
-                                <div className="mb-2">
-                                    <ul className="font-size-14 pl-3 ml-1 text-gray-110">
+                                <div className="mb-4">
+                                    <ul className="list-disc font-thin space-y-1 pl-3 ml-1">
                                         <li>4.5 inch HD Touch Screen (1280 x 720)</li>
                                         <li>Android 4.4 KitKat OS</li>
                                         <li>1.4 GHz Quad Core™ Processor</li>
@@ -100,11 +103,13 @@ function ProductDetails({}: Props) {
                                     </ul>
                                 </div>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
-                                <p><strong>SKU</strong>: FW511948218</p>
+
+                                <p className='my-10'><span className='font-bold text-gray-700 '>SKU</span>:   FW511948218</p>
+
                                 <div className="mb-4">
-                                    <div className="d-flex align-items-baseline">
-                                        <ins className="font-size-36 text-decoration-none">$1,999.00</ins>
-                                        <del className="font-size-20 ml-2 text-gray-6">$2,299.00</del>
+                                    <div className="flex flex-row items-baseline ">
+                                        <p className="text-5xl text-black/60">$1,999.00</p>
+                                        <del className="text-xl ml-2 font-medium text-gray-500">$2,299.00</del>
                                     </div>
                                 </div>
                                 <div className="border-top border-bottom py-3 mb-4">
