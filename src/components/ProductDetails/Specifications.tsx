cc28005d6a3e809cf1e6b5e126784a3cb5335b46
Jobsidney <jobsidney67@gmail.com>
2023-09-22 10:37:14 +0300

    ecification done

--- src/components/ProductDetails/Specifications.tsx
+++ src/components/ProductDetails/Specifications.tsx
@@ -4,117 +4,113 @@ type Props = {}
 
 function Specifications({}: Props) {
   return (
-    <div>
-         {/* <div className="tab-pane fade" id="Jpills-three-example1" role="tabpanel" aria-labelledby="Jpills-three-example1-tab">
-                                <div className="mx-md-5 pt-1">
-                                    <div className="table-responsive mb-4">
-                                        <table className="table table-hover">
-                                            <tbody>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5 border-top-0">Weight</th>
-                                                    <td className="border-top-0">7.25kg</td>
+  <div className="w-full lg:px-[5%] space-y-7">
+                                    
+                                        <table className="table table-hover w-full">
+                                            <tbody className='divide-y text-left'>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Weight</th>
+                                                    <td className="px-3 py-3">7.25kg</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Dimensions</th>
-                                                    <td>90 x 60 x 90 cm</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Dimensions</th>
+                                                    <td className="px-3 py-3">90 x 60 x 90 cm</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Size</th>
-                                                    <td>One Size Fits all</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Size</th>
+                                                    <td className="px-3 py-3">One Size Fits all</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">color</th>
-                                                    <td>Black with Red, White with Gold</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">color</th>
+                                                    <td className="px-3 py-3">Black with Red, White with Gold</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Guarantee</th>
-                                                    <td>5 years</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Guarantee</th>
+                                                    <td className="px-3 py-3">5 years</td>
                                                 </tr>
                                             </tbody>
                                         </table>
-                                    </div>
-                                    <h3 className="font-size-18 mb-4">Technical Specifications</h3>
+                                   
+                                    <h3 className="text-2xl mb-4 text-black/70">Technical Specifications</h3>
                                     <div className="table-responsive">
-                                        <table className="table table-hover">
-                                            <tbody>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5 border-top-0">Brand</th>
-                                                    <td className="border-top-0">Apple</td>
+                                        <table className="table table-hover w-full">
+                                            <tbody className='divide-y text-left'>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Brand</th>
+                                                    <td className="px-3 py-3">Apple</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Item Height</th>
-                                                    <td>18 Millimeters</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Item Height</th>
+                                                    <td className="px-3 py-3">18 Millimeters</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Item Width</th>
-                                                    <td>31.4 Centimeters</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Item Width</th>
+                                                    <td className="px-3 py-3">31.4 Centimeters</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Screen Size</th>
-                                                    <td>13 Inches</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Screen Size</th>
+                                                    <td className="px-3 py-3">13 Inches</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Item Weight</th>
-                                                    <td>1.6 Kg</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Item Weight</th>
+                                                    <td className="px-3 py-3">1.6 Kg</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Product Dimensions</th>
-                                                    <td>21.9 x 31.4 x 1.8 cm</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Product Dimensions</th>
+                                                    <td className="px-3 py-3">21.9 x 31.4 x 1.8 cm</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Item model number</th>
-                                                    <td>MF841HN/A</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Item model number</th>
+                                                    <td className="px-3 py-3">MF841HN/A</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Processor Brand</th>
-                                                    <td>Intel</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Processor Brand</th>
+                                                    <td className="px-3 py-3">Intel</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Processor Type</th>
-                                                    <td>Core i5</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Processor Type</th>
+                                                    <td className="px-3 py-3">Core i5</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Processor Speed</th>
-                                                    <td>2.9 GHz</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Processor Speed</th>
+                                                    <td className="px-3 py-3">2.9 GHz</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">RAM Size</th>
-                                                    <td>8 GB</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">RAM Size</th>
+                                                    <td className="px-3 py-3">8 GB</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Hard Drive Size</th>
-                                                    <td>512 GB</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Hard Drive Size</th>
+                                                    <td className="px-3 py-3">512 GB</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Hard Disk Technology</th>
-                                                    <td>Solid State Drive</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Hard Disk Technology</th>
+                                                    <td className="px-3 py-3">Solid State Drive</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Graphics Coprocessor</th>
-                                                    <td>Intel Integrated Graphics</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Graphics Coprocessor</th>
+                                                    <td className="px-3 py-3">Intel Integrated Graphics</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Graphics Card Description</th>
-                                                    <td>Integrated Graphics Card</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Graphics Card Description</th>
+                                                    <td className="px-3 py-3">Integrated Graphics Card</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Hardware Platform</th>
-                                                    <td>Mac</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Hardware Platform</th>
+                                                    <td className="px-3 py-3">Mac</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Operating System</th>
-                                                    <td>Mac OS</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Operating System</th>
+                                                    <td className="px-3 py-3">Mac OS</td>
                                                 </tr>
-                                                <tr>
-                                                    <th className="px-4 px-xl-5">Average Battery Life (in hours)</th>
-                                                    <td>9</td>
+                                                <tr className='hover:bg-slate-100'>
+                                                    <th className=" pl-6 py-2 text-black/70">Average Battery Life (in hours)</th>
+                                                    <td className="px-3 py-3">9</td>
                                                 </tr>
                                             </tbody>
                                         </table>
                                     </div>
                                 </div>
-                            </div> */}
-    </div>
   )
 }
 
