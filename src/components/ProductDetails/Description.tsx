4f70ae7fc40f2483dbfa4141e119f75986fd1d5a
Jobsidney <jobsidney67@gmail.com>
2023-09-22 10:02:39 +0300

    descriptio done

--- src/components/ProductDetails/Description.tsx
+++ src/components/ProductDetails/Description.tsx
@@ -5,44 +5,48 @@ type Props = {}
 function Description({}: Props) {
   return (
     <div>
-        <div className="borders-radius-17 border p-4 mt-4 mt-md-0 px-lg-10 py-lg-9">
+        <div className="borders-radius-17  p-4 mt-4 mt-md-0 px-lg-10 py-lg-9">
                         <div className="tab-content" id="Jpills-tabContent">
                            
                             <div className="tab-pane fade active show" id="Jpills-two-example1" role="tabpanel" aria-labelledby="Jpills-two-example1-tab">
-                                <h3 className="font-size-24 mb-3">Perfectly Done</h3>
+                                <h3 className="text-3xl text-black/70 mb-4">Perfectly Done</h3>
                                 <p>Praesent ornare, ex a interdum consectetur, lectus diam sodales elit, vitae egestas est enim ornare nisl. Nullam in lectus nec sem semper viverra. In lobortis egestas massa. Nam nec massa nisi. Suspendisse potenti. Quisque suscipit vulputate dui quis volutpat. Ut id elit facilisis, feugiat est in, tempus lacus. Ut ultrices dictum metus, a ultricies ex vulputate ac. Ut id cursus tellus, non tempor quam. Morbi porta diam nisi, id finibus nunc tincidunt eu.</p>
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <div className="pt-lg-8 pt-xl-10">
-                                            <h3 className="font-size-24 mb-3">Wireless</h3>
-                                            <p className="mb-6">Fusce vitae nibh mi. Integer posuere, libero et ullamcorper facilisis, enim eros tincidunt orci, eget vestibulum sapien nisi ut leo. Cras finibus vel est ut mollis. Donec luctus condimentum ante et euismod.</p>
-                                            <h3 className="font-size-24 mb-3">Fresh Design</h3>
-                                            <p className="mb-6">Integer bibendum aliquet ipsum, in ultrices enim sodales sed. Quisque ut urna vitae lacus laoreet malesuada eu at massa. Pellentesque nibh augue, pellentesque nec dictum vel, pretium a arcu. Duis eu urna suscipit, lobortis elit quis, ullamcorper massa.</p>
-                                            <h3 className="font-size-24 mb-3">Fabolous Sound</h3>
-                                            <p className="mb-6">Cras rutrum, nibh a sodales accumsan, elit sapien ultrices sapien, eget semper lectus ex congue elit. Nullam dui elit, fermentum a varius at, iaculis non dolor. In hac habitasse platea dictumst.</p>
+                                <div className="">
+                                    <div className='grid grid-cols-2 gap-24 '>
+                                       
+                                            <div className="flex flex-col justify-center">
+                                                <h3 className="text-3xl text-black/70 mb-4">Wireless</h3>
+                                                <p className="mb-6">Fusce vitae nibh mi. Integer posuere, libero et ullamcorper facilisis, enim eros tincidunt orci, eget vestibulum sapien nisi ut leo. Cras finibus vel est ut mollis. Donec luctus condimentum ante et euismod.</p>
+                                                <h3 className="text-3xl text-black/70 mb-4">Fresh Design</h3>
+                                                <p className="mb-6">Integer bibendum aliquet ipsum, in ultrices enim sodales sed. Quisque ut urna vitae lacus laoreet malesuada eu at massa. Pellentesque nibh augue, pellentesque nec dictum vel, pretium a arcu. Duis eu urna suscipit, lobortis elit quis, ullamcorper massa.</p>
+                                                <h3 className="text-3xl text-black/70 mb-4">Fabolous Sound</h3>
+                                                <p className="mb-6">Cras rutrum, nibh a sodales accumsan, elit sapien ultrices sapien, eget semper lectus ex congue elit. Nullam dui elit, fermentum a varius at, iaculis non dolor. In hac habitasse platea dictumst.</p>
+                                            </div>
+                                       
+                                        <div className="col-md-6 text-right bg-red-900">
+                                            <img className="w-[80rem]" src="https://transvelo.github.io/electro-html/2.0/assets/img/580X580/img1.jpg" alt="Image Description"/>
                                         </div>
                                     </div>
-                                    <div className="col-md-6 text-right">
-                                        <img className="img-fluid mr-n4 mr-lg-n10" src="../../assets/img/580X580/img1.jpg" alt="Image Description"/>
-                                    </div>
-                                    <div className="col-md-6 text-left">
-                                        <img className="img-fluid ml-n4 ml-lg-n10" src="../../assets/img/580X580/img2.jpg" alt="Image Description"/>
-                                    </div>
-                                    <div className="col-md-6 align-self-center">
-                                        <div className="pt-lg-8 pt-xl-10 text-right">
-                                            <h3 className="font-size-24 mb-3">Inteligent Bass</h3>
-                                            <p className="mb-6">Fusce vitae nibh mi. Integer posuere, libero et ullamcorper facilisis, enim eros tincidunt orci, eget vestibulum sapien nisi ut leo. Cras finibus vel est ut mollis. Donec luctus condimentum ante et euismod.</p>
-                                            <h3 className="font-size-24 mb-3">Battery Life</h3>
-                                            <p className="mb-6">Integer bibendum aliquet ipsum, in ultrices enim sodales sed. Quisque ut urna vitae lacus laoreet malesuada eu at massa. Pellentesque nibh augue, pellentesque nec dictum vel, pretium a arcu. Duis eu urna suscipit, lobortis elit quis, ullamcorper massa.</p>
+
+                                    <div className='grid grid-cols-2 gap-24 justify-between'>
+                                        <div className="">
+                                            <img className="w-[80rem]" src="https://transvelo.github.io/electro-html/2.0/assets/img/580X580/img2.jpg" alt="Image Description"/>
                                         </div>
+                                        
+                                        <div className="flex flex-col justify-center">
+                                                <h3 className="text-3xl text-black/70 mb-4">Inteligent Bass</h3>
+                                                <p className="mb-6">Fusce vitae nibh mi. Integer posuere, libero et ullamcorper facilisis, enim eros tincidunt orci, eget vestibulum sapien nisi ut leo. Cras finibus vel est ut mollis. Donec luctus condimentum ante et euismod.</p>
+                                                <h3 className="text-3xl text-black/70 mb-4">Battery Life</h3>
+                                                <p className="mb-6">Integer bibendum aliquet ipsum, in ultrices enim sodales sed. Quisque ut urna vitae lacus laoreet malesuada eu at massa. Pellentesque nibh augue, pellentesque nec dictum vel, pretium a arcu. Duis eu urna suscipit, lobortis elit quis, ullamcorper massa.</p>
+                                            </div>
+                                        
                                     </div>
                                 </div>
-                                <ul className="nav flex-nowrap flex-xl-wrap overflow-auto overflow-xl-visble">
+                                <ul className="flex flex-row">
                                     <li className="nav-item text-gray-111 flex-shrink-0 flex-xl-shrink-1"><strong>SKU:</strong> <span className="sku">FW511948218</span></li>
                                     <li className="nav-item text-gray-111 mx-3 flex-shrink-0 flex-xl-shrink-1">/</li>
-                                    <li className="nav-item text-gray-111 flex-shrink-0 flex-xl-shrink-1"><strong>Category:</strong> <a href="#" className="text-blue">Headphones</a></li>
-                                    <li className="nav-item text-gray-111 mx-3 flex-shrink-0 flex-xl-shrink-1">/</li>
-                                    <li className="nav-item text-gray-111 flex-shrink-0 flex-xl-shrink-1"><strong>Tags:</strong> <a href="#" className="text-blue">Fast</a>, <a href="#" className="text-blue">Gaming</a>, <a href="#" className="text-blue">Strong</a></li>
+                                    <li className="nav-item text-gray-111 flex-shrink-0 flex-xl-shrink-1"><strong>Category:</strong> Headphones</li>
+                                    
                                 </ul>
                             </div>
                         </div>
