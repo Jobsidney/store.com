8e6909d39d03d58cb9275444398fb49832093520
Jobsidney <jobsidney67@gmail.com>
2023-09-22 09:14:53 +0300

    new specification component

---
+++ src/components/ProductDetails/Specifications.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+
+type Props = {}
+
+function Specifications({}: Props) {
+  return (
+    <div>
+         {/* <div className="tab-pane fade" id="Jpills-three-example1" role="tabpanel" aria-labelledby="Jpills-three-example1-tab">
+                                <div className="mx-md-5 pt-1">
+                                    <div className="table-responsive mb-4">
+                                        <table className="table table-hover">
+                                            <tbody>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5 border-top-0">Weight</th>
+                                                    <td className="border-top-0">7.25kg</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Dimensions</th>
+                                                    <td>90 x 60 x 90 cm</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Size</th>
+                                                    <td>One Size Fits all</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">color</th>
+                                                    <td>Black with Red, White with Gold</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Guarantee</th>
+                                                    <td>5 years</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <h3 className="font-size-18 mb-4">Technical Specifications</h3>
+                                    <div className="table-responsive">
+                                        <table className="table table-hover">
+                                            <tbody>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5 border-top-0">Brand</th>
+                                                    <td className="border-top-0">Apple</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Item Height</th>
+                                                    <td>18 Millimeters</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Item Width</th>
+                                                    <td>31.4 Centimeters</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Screen Size</th>
+                                                    <td>13 Inches</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Item Weight</th>
+                                                    <td>1.6 Kg</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Product Dimensions</th>
+                                                    <td>21.9 x 31.4 x 1.8 cm</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Item model number</th>
+                                                    <td>MF841HN/A</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Processor Brand</th>
+                                                    <td>Intel</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Processor Type</th>
+                                                    <td>Core i5</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Processor Speed</th>
+                                                    <td>2.9 GHz</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">RAM Size</th>
+                                                    <td>8 GB</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Hard Drive Size</th>
+                                                    <td>512 GB</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Hard Disk Technology</th>
+                                                    <td>Solid State Drive</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Graphics Coprocessor</th>
+                                                    <td>Intel Integrated Graphics</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Graphics Card Description</th>
+                                                    <td>Integrated Graphics Card</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Hardware Platform</th>
+                                                    <td>Mac</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Operating System</th>
+                                                    <td>Mac OS</td>
+                                                </tr>
+                                                <tr>
+                                                    <th className="px-4 px-xl-5">Average Battery Life (in hours)</th>
+                                                    <td>9</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div> */}
+    </div>
+  )
+}
+
+export default Specifications
