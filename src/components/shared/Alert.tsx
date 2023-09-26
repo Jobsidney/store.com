679c92e3a8b962e21cd63fc20eafe15de31d6146
Jobsidney <jobsidney67@gmail.com>
2023-09-26 12:10:50 +0300

    aleart change sizing

--- src/components/shared/Alert.tsx
+++ src/components/shared/Alert.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 function Alert({}: Props) {
   return (
-    <div  className="bg-[#fed700] py-5 px-2 border-l-8 border-black/20 text-lg px-6" role="alert">
+    <div  className="bg-[#fed700] py-4 my-6 px-2 border-l-8 border-black/20  " role="alert">
     Returning customer? 
-    <a href="#" className="font-bold px-1" data-toggle="collapse" data-target="#shopCartOne" aria-expanded="false" aria-controls="shopCartOne">Click here to login</a>
+    <a href="#" className="font-semibold px-1" data-toggle="collapse" data-target="#shopCartOne" aria-expanded="false" aria-controls="shopCartOne">Click here to login</a>
 </div>
   )
 }
